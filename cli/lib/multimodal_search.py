e236747839ffe882cc28f1f23e04fe2d76ec2270
Giovanni Flore <flore.giovanni.scripting@outlook.com>
2026-07-16 17:20:08 +0200

    completion

--- cli/lib/multimodal_search.py
+++ cli/lib/multimodal_search.py
@@ -1,15 +1,47 @@
 from PIL import Image
 from sentence_transformers import SentenceTransformer
+from .search_utils import load_movies
+from .semantic_search import cosine_similarity
 
 class MultimodalSearch:
-    def __init__(self, model_name = "clip-ViT-B-32") -> None:
+    def __init__(self, documents: list = [], model_name = "clip-ViT-B-32") -> None:
         self.model = SentenceTransformer(model_name_or_path=model_name)
+        self.documents = documents
+        self.texts: list[str] = []
+
+        for d in documents:
+            self.texts.append(f"{d['title']}: {d['description']}")
+
+        self.text_embeddings = self.model.encode(self.texts,show_progress_bar=True)
 
     def embed_image(self, img_path: str):
         img = Image.open(img_path)
         return self.model.encode(img)
 
+    def search_from_image(self, img_path: str) -> list[dict]:
+        img = Image.open(img_path)
+        img_emb = self.model.encode(img)
+        cos_sim_list = []
+        out = []
+
+        for e_t in self.text_embeddings:
+            cos_sim_list.append(cosine_similarity(e_t,img_emb))
+
+        for i in range(len(cos_sim_list)):
+            d = self.documents[i]
+            out.append({
+                "id": d['id'],
+                "title": d['title'],
+                "description": d['description'],
+                "score": cos_sim_list[i],
+            })
+        return sorted(out,key=lambda item: item['score'], reverse=True)[:5]
+
 def verify_image_embedding(img_path: str):
     mmsearch = MultimodalSearch()
     embedding = mmsearch.embed_image(img_path)
     print(f"Embedding shape: {embedding.shape[0]} dimensions")
+
+def search_by_image(img_path: str) -> list[dict]:
+    mms = MultimodalSearch(documents=load_movies())
+    return mms.search_from_image(img_path)
--- cli/multimodal_search_cli.py
+++ cli/multimodal_search_cli.py
@@ -1,5 +1,5 @@
 import argparse
-from lib.multimodal_search import verify_image_embedding
+from lib.multimodal_search import verify_image_embedding, search_by_image
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Multimodal Search CLI")
@@ -10,6 +10,11 @@ def main() -> None:
     )
     vie_parser.add_argument("image_path", type=str, help="path to the image to embed.")
 
+    img_search_parser = subparsers.add_parser(
+        "image_search", help="Search movies by image."
+    )
+    img_search_parser.add_argument("image_path", type=str, help="path to the source image.")
+
     args = parser.parse_args()
 
     match args.command:
@@ -17,6 +22,11 @@ def main() -> None:
             if args.image_path != "":
                 verify_image_embedding(args.image_path)
 
+        case "image_search":
+            if args.image_path != "":
+                results = search_by_image(args.image_path)
+                for i,r in enumerate(results):
+                    print(f"{i}. {r['title']} (similarity: {r['score']:.3f})\n{r['description'][:100]}\n")
         case _:
             parser.print_help()
 
